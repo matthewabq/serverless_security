de888a6c15df5805737436e391521d040347c1d5
matthewabq <mbarker@paloaltonetworks.com>
2020-02-26 11:32:57 -0700

    minor changes

--- lambda/subprocess/main.py
+++ lambda/subprocess/main.py
@@ -3,12 +3,14 @@ import os
 import json
 # import twistlock.serverless
 
+# ---------------------------------------------------------
 # To protect this lambda function with Prisma Cloud Compute:
 #   - create policy in Defend/Runtime/Serverless
 #   - create policy in Defend/Firewalls/CloudNativeAppFirewall/Serverless
 #   - download python based serverless defender as zip file here and unzip
 #   - remove the comments for the 2 lines with 'twistlock' in them 
 #   - zip up everything but the zip file (use -r option) and upload zip file to your new lambda function in AWS
+# ----------------------------------------------------------
 
 # @twistlock.serverless.handler
 def lambda_handler(event, context):
